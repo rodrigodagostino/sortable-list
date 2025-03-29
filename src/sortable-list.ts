06e6d8458690aff3e8027af41bae0409dd669bd0
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2025-03-29 02:49:24 -0300

    feat: display ghost element on <sortable-item> drag

--- src/sortable-list.ts
+++ src/sortable-list.ts
@@ -1,4 +1,7 @@
 class SortableList extends HTMLElement {
+	pointerOrigin: { x: number; y: number } = { x: 0, y: 0 };
+	ghost: HTMLElement = document.createElement('div');
+
 	constructor() {
 		super();
 		const shadow = this.attachShadow({ mode: 'open' });
@@ -12,6 +15,54 @@ class SortableList extends HTMLElement {
 			</style>
 			<slot></slot>
 		`;
+
+		this.handlePointerDown = this.handlePointerDown.bind(this);
+		this.handlePointerMove = this.handlePointerMove.bind(this);
+		this.handlePointerUp = this.handlePointerUp.bind(this);
+	}
+
+	handlePointerDown(event: PointerEvent) {
+		const target = event.target as HTMLElement;
+		const currItem: HTMLElement | null = target.closest('sortable-item');
+
+		if (!currItem) return;
+
+		this.pointerOrigin = { x: event.clientX, y: event.clientY };
+		const clone = currItem.cloneNode(true) as HTMLElement;
+		this.ghost.style.position = 'fixed';
+		this.ghost.style.left = `${currItem.getBoundingClientRect().x}px`;
+		this.ghost.style.top = `${currItem.getBoundingClientRect().y}px`;
+		this.ghost.appendChild(clone);
+		document.body.appendChild(this.ghost);
+		currItem?.setPointerCapture(event.pointerId);
+
+		currItem.addEventListener('pointermove', this.handlePointerMove);
+		currItem.addEventListener(
+			'pointerup',
+			() => {
+				currItem.removeEventListener('pointermove', this.handlePointerMove);
+				this.handlePointerUp();
+			},
+			{ once: true }
+		);
+	}
+
+	handlePointerMove(event: PointerEvent) {
+		this.ghost.style.transform = `translate3d(${event.clientX - this.pointerOrigin.x}px, ${event.clientY - this.pointerOrigin.y}px, 0)`;
+	}
+
+	handlePointerUp() {
+		this.ghost.removeAttribute('style');
+		this.ghost.replaceChildren();
+		this.ghost.remove();
+	}
+
+	connectedCallback() {
+		this.addEventListener('pointerdown', this.handlePointerDown);
+	}
+
+	disconnectedCallback() {
+		this.removeEventListener('pointerdown', this.handlePointerDown);
 	}
 }
 
